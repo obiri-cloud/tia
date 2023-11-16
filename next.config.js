4881109dc9a93d1601c8365b5fa36ad9e3066fcc
sylvester-tiacloud <sylvester.sarpong@tiacloud.io>
2023-11-16 20:56:38 +0000

    misc: troubleshooting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['tialabs-api.tiapod.tiacloud.dev']
-      },
-}
+  images: {
+    domains: ["tialabs-api.tiapod.tiacloud.dev"],
+  },
+  env: {
+    NEXT_PUBLIC_BE_URL: process.env.NEXT_PUBLIC_BE_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
