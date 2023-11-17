7fa62acf65b2519c6ff502ceae3beeba8c8e0930
sylvester-tiacloud <sylvester.sarpong@tiacloud.io>
2023-11-17 10:35:16 +0000

    cleanup: console logs removed

--- app/components/theme-provider-wrapper.tsx
+++ app/components/theme-provider-wrapper.tsx
@@ -6,9 +6,5 @@ export default function ThemeProviderWrapper({
 }: {
   children: React.ReactNode;
 }): React.ReactNode {
-  console.log("NEXT_PUBLIC_BE_URL", process.env.NEXT_PUBLIC_BE_URL);
-  console.log("NEXTAUTH_SECRET", process.env.NEXTAUTH_SECRET);
-  console.log("NEXTAUTH_URL", process.env.NEXTAUTH_URL);
-  
   return <ThemeProvider attribute="class">{children}</ThemeProvider>;
 }
