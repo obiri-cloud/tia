2b8314178f5372759c578762fb9b4e60f579d186
sylvester-tiacloud <sylvester.sarpong@tiacloud.io>
2023-12-04 20:16:12 +0000

    update: protecting the admin page

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(function middleware(req) {}, {
   callbacks: {
     authorized: ({ req, token }) => {
-      if (req.nextUrl.pathname.startsWith("/dashboard") && token === null) {
+      if (req.nextUrl.pathname.startsWith("/dashboard") && req.nextUrl.pathname.startsWith("/admin")  && token === null) {
         return false;
       }
       return true;
