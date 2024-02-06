bdc3980efa0d98f47190f50035222655d109e8f1
tylerasa <sarpongasaresylvester@gmail.com>
2024-02-06 11:33:21 +0000

    fix: overflow fixed

--- app/dashboard/labs/page.tsx
+++ app/dashboard/labs/page.tsx
@@ -363,7 +363,7 @@ const LabsPage = () => {
             <Drawer.Portal>
               <Drawer.Overlay className="fixed inset-0 bg-black/40" />
               <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] h-[96%] mt-24 fixed bottom-0 left-0 right-0">
-                <div className=" bg-white rounded-t-[10px] flex-1">
+                <div className=" bg-white rounded-t-[10px] flex-1 overflow-y-scroll">
                   <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
                   <div className="max-w-md mx-auto">
                     <Instructions instructions={instructions} />
