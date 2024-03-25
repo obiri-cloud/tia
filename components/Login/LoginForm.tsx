0c8729f9487a38d618ef50a1dd8e305cc3dc5689
tylerasa <sarpongasaresylvester@gmail.com>
2024-03-25 03:31:41 +0000

    style: dark text fixed

--- components/Login/LoginForm.tsx
+++ components/Login/LoginForm.tsx
@@ -158,7 +158,9 @@ const LoginForm = () => {
             href="/signup"
             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
           >
-            Don't have any account? Sign up
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              Don't have any account? Sign up
+            </span>
             <BottomGradient />
           </Link>
           <Link
--- components/Signup/SForm.tsx
+++ components/Signup/SForm.tsx
@@ -208,7 +208,9 @@ export function SignupFormDemo() {
             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
             type="submit"
           >
-            Login
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              Login
+            </span>
             <BottomGradient />
           </button>
           <button
