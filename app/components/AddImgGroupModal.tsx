fa19f50788a13fefc9c12383899df13f0e78850a
tylerasa <sarpongasaresylvester@gmail.com>
2024-05-02 11:36:44 +0000

    refactor: using the orgdialog

--- app/components/AddImgGroupModal.tsx
+++ app/components/AddImgGroupModal.tsx
@@ -1,12 +1,11 @@
+"use client";
 
-"use client"
- 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { DialogContent } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import axios from "axios";
 import {
   Form,
@@ -16,92 +15,70 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from "@/components/ui/form";
 import { useRouter } from "next/router";
 import { useRef, useState } from "react";
-import { toast } from "@/components/ui/use-toast"
-import { useSession } from "next-auth/react"; 
-
-const items = [
-  {
-    id: "recents",
-    label: "Recents",
-  },
-  {
-    id: "home",
-    label: "Home",
-  },
-  {
-    id: "applications",
-    label: "Applications",
-  },
-  {
-    id: "desktop",
-    label: "Desktop",
-  },
-  {
-    id: "downloads",
-    label: "Downloads",
-  },
-  {
-    id: "documents",
-    label: "Documents",
-  },
-] 
+import { toast } from "@/components/ui/use-toast";
+import { useSession } from "next-auth/react";
+import OrgDialog from "./my-organization/org-dialog";
+import { GroupMember, ILabImage } from "../types";
 
 const FormSchema = z.object({
   image: z.array(z.number()).refine((value) => value.some((item) => item), {
     message: "You have to select at least one item.",
   }),
-})
+});
 
-
- 
- function CheckboxReactHookFormMultiple(image:any,gid:number) {
-  const buttonRef = useRef<HTMLButtonElement>(null);
-
-  console.log(image.gid);
-
-  
+function AddImgGroupModal({
+  image,
+  gid,
+  onSubmit,
+}: {
+  image: ILabImage[] | undefined;
+  gid: number | undefined;
+  onSubmit: () => void;
+}) {
+  console.log("image", image);
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       image: [],
     },
-  })
+  });
 
-
-    
   const { data: session } = useSession();
   // @ts-ignore
   const token = session?.user!.tokens?.access_token;
- 
 
- async function onSubmit(data:z.infer<typeof FormSchema>){
-  let image_ids = { image_ids: data.image }
+  async function handleSubmit(data: z.infer<typeof FormSchema>) {
+    let image_ids = { image_ids: data.image };
 
-
+    (
+      document.getElementById("add-image-to-grp-button") as HTMLButtonElement
+    ).disabled = true;
+    (
+      document.getElementById("add-image-to-grp-button") as HTMLButtonElement
+    ).textContent = "Adding Images..."
+    
     let axiosConfig = {
       method: "POST",
-      url: `${process.env.NEXT_PUBLIC_BE_URL}/organization/group/${image.gid}/image/add/`,
-      data:image_ids,
+      url: `${process.env.NEXT_PUBLIC_BE_URL}/organization/group/${gid}/image/add/`,
+      data: image_ids,
       headers: {
         "Content-Type": "application/json",
-        // "Content-Type": "multipart/form-data",
         Authorization: `Bearer ${token}`,
-      }
+      },
     };
     try {
       const response = await axios(axiosConfig);
-      console.log({response});
       if (response.status === 201 || response.status === 200) {
         toast({
           variant: "success",
           title: `Image added to Group sucessfully`,
-          description: ``,
+          duration: 2000,
         });
-        // router.push(`/my-organization/groups`);
+        onSubmit();
       } else {
         toast({
           variant: "destructive",
@@ -109,94 +86,92 @@ const FormSchema = z.object({
           description: response.data,
         });
       }
-    } catch (error:any) {
+    } catch (error: any) {
       console.error("error", error);
       const responseData = error.response.data;
       if (error.response) {
         toast({
           variant: "destructive",
           title: `${responseData.data}`,
-          // description: responseData.message || "An error occurred",
         });
       } else {
         toast({
           variant: "destructive",
           title: `${responseData.data}`,
-          // description: error.message || "An error occurred",
         });
       }
     } finally {
-      if (buttonRef.current) {
-        buttonRef.current.disabled = false;
-      }
+      (
+        document.getElementById("add-image-to-grp-button") as HTMLButtonElement
+      ).disabled = false;
+      (
+        document.getElementById("add-image-to-grp-button") as HTMLButtonElement
+      ).textContent = "Add Images";
     }
-  };
+  }
 
-
-
-
- 
   return (
-    <DialogContent
-      className="flex justify-center items-center overflow-y-scroll h-[60vh] "
+    <OrgDialog
+      title="Group Images"
+      description="Select the Images you want to add to the group"
     >
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="image"
-          render={() => (
-            <FormItem>
-              <div className="mb-4">
-                <FormLabel className="text-base">Organization Images</FormLabel>
-                <FormDescription>
-                  Select the Images you want to add to the group
-                </FormDescription>
-              </div>
-              {image.image.map((item:any) => (
-                <FormField
-                  key={item.id}
-                  control={form.control}
-                  name="image"
-                  render={({ field }) => {
-                    return (
-                      <FormItem
-                        key={item.id}
-                        className="flex flex-row items-start space-x-3 space-y-0"
-                      >
-                        <FormControl>
-                          <Checkbox
-                        
-                            checked={field.value?.includes(item.id)}
-                            onCheckedChange={(checked:any) => {
-                              console.log(checked,field);
-                              return checked
-                                ? field.onChange([...field.value,item.id])
-                                : field.onChange(
-                                    field.value?.filter(
-                                      (value) => value !== item.id
-                                    )
-                                  )
-                            }}
-                          />
-                        </FormControl>
-                        <FormLabel className="text-sm font-normal">
-                          {item.name}
-                        </FormLabel>
-                      </FormItem>
-                    )
-                  }}
-                />
-              ))}
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-    </DialogContent>
-  )
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="image"
+            render={() => (
+              <FormItem>
+                {image &&
+                  image.map((item: ILabImage) => (
+                    <FormField
+                      key={item.id}
+                      control={form.control}
+                      name="image"
+                      render={({ field }) => {
+                        return (
+                          <FormItem
+                            key={item.id}
+                            className="flex flex-row items-start space-x-3 space-y-0"
+                          >
+                            <FormLabel className="flex items-center space-x-4">
+                              <Checkbox
+                                checked={field.value?.includes(item.id)}
+                                onCheckedChange={(checked: any) => {
+                                  console.log(checked, field);
+                                  return checked
+                                    ? field.onChange([...field.value, item.id])
+                                    : field.onChange(
+                                        field.value?.filter(
+                                          (value) => value !== item.id
+                                        )
+                                      );
+                                }}
+                              />
+                              <div className="bg-muted text-black dark:text-white font-bold p-4 w-4 h-4 flex justify-center items-center uppercase  rounded-full">
+                                {item.name[0]}
+                              </div>
+                              <div className="flex-1">
+                                <div className="font-medium text-black dark:text-white ">
+                                  {`${item.name} `}
+                                </div>
+                              </div>
+                            </FormLabel>
+                          </FormItem>
+                        );
+                      }}
+                    />
+                  ))}
+              </FormItem>
+            )}
+          />
+          <Button id="add-image-to-grp-button" type="submit" className="w-full">
+            Add Images
+          </Button>
+        </form>
+      </Form>
+    </OrgDialog>
+  );
 }
 
-export default CheckboxReactHookFormMultiple
+export default AddImgGroupModal;
--- app/components/CreateGroupModal.tsx
+++ app/components/CreateGroupModal.tsx
@@ -1,188 +1,49 @@
 "use client";
-import React, { FC, FormEvent, useEffect, useRef, useState } from "react";
+import React, { FormEvent } from "react";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import { DialogContent } from "@/components/ui/dialog";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
-import * as z from "zod";
-import axios from "axios";
-import { toast } from "@/components/ui/use-toast";
-import { useSession } from "next-auth/react";
-// import { getImageListX } from "./overview";
-import { getImageListX } from "./admin/overview";
-import {
-  setCurrentImage,
-  setImageCount,
-  setImageList,
-} from "@/redux/reducers/adminSlice";
-import { useDispatch, useSelector } from "react-redux";
-import trash from "@/public/svgs/trash.svg";
-import Image from "next/image";
-import { RootState } from "@/redux/store";
-import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
-import { ContentProps, ILabImage } from "@/app/types";
-import { Trash } from "lucide-react";
-import { useRouter } from "next/navigation";
+import OrgDialog from "./my-organization/org-dialog";
 
-const NewImageForm = () => {
+const CreateGroupModal = ({
+  onSubmit,
+}: {
+  onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+}) => {
   const form = useForm();
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  const router = useRouter();
-  const dispatch = useDispatch();
 
-  const nameRef = useRef<HTMLInputElement>(null);
-  const { data: session } = useSession();
-  // @ts-ignore
-  const token = session?.user!.tokens?.access_token;
-
-
-
-  
-
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    let name= nameRef.current?.value;
-
-    const formSchema = z.object({
-      name: z.string()
-    });
-
-    if (buttonRef.current) {
-      buttonRef.current.disabled = true;
-    }
-
-    let parseFormData = {
-      name: nameRef.current?.value,
-    };
-
-    const formData = new FormData();
-
-    // Append fields to the FormData object
-    formData.append("name", nameRef.current?.value || "");
-
-    // }
-
-    let axiosConfig = {
-      method: "POST",
-      url: `${process.env.NEXT_PUBLIC_BE_URL}/organization/group/create/`,
-      data:formData,
-      headers: {
-        "Content-Type": "application/json",
-        // "Content-Type": "multipart/form-data",
-        Authorization: `Bearer ${token}`,
-      }
-    };
-
-    try {
-      formSchema.parse(parseFormData);
-      const response = await axios(axiosConfig);
-      console.log({response});
-      
-
-      if (response.status === 201 || response.status === 200) {
-        toast({
-          variant: "success",
-          title: `group created sucessfully`,
-          description: ``,
-        });
-        router.push(`/my-organization/groups`);
-      } else {
-        toast({
-          variant: "destructive",
-          title: "Invitation  Creation  Error",
-          description: response.data.data,
-        });
-      }
-    } catch (error:any) {
-      console.error("error", error);
-      const responseData = error.response.data;
-      if (error.response) {
-        toast({
-          variant: "destructive",
-          title: `${responseData.data}`,
-          // description: responseData.message || "An error occurred",
-        });
-      } else {
-        toast({
-          variant: "destructive",
-          title: `${responseData.data}`,
-          // description: error.message || "An error occurred",
-        });
-      }
-    } finally {
-      if (buttonRef.current) {
-        buttonRef.current.disabled = false;
-      }
-    }
-  };
-
-  function removeNullFields(obj: any) {
-    for (const key in obj) {
-      if (obj[key] === "null") {
-        delete obj[key];
-      }
-    }
-    return obj;
-  }
-
-
-
-
-  const handleOnEsc = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === "Escape") {
-      dispatch(setCurrentImage(null));
-    }
-  };
-
-  const handleOnClickOutside = (e: ContentProps["onPointerDownOutside"]) => {
-    dispatch(setCurrentImage(null));
-  };
 
   return (
-    <DialogContent
-      onEsc={(e) => handleOnEsc(e)}
-      onClickOutside={(e) => handleOnClickOutside(e)}
-      className="flex justify-center items-center overflow-y-scroll h-[40vh] "
+    <OrgDialog
+      title="Create a group"
+      description="Separete your members into manageable groups"
     >
       <Form {...form}>
         <form
-          onSubmit={handleSubmit}
-          className="max-w-[500px] container w-full dark:text-white text-black"
+        id="create-group-form"
+          onSubmit={(e) => onSubmit(e)}
+          className=" w-full dark:text-white text-black"
         >
           <FormField
             control={form.control}
             name="name"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className=" formTextLight">
-                  Name Of Group
-                </FormLabel>
                 <FormControl>
                   <Input
+                    id="group-name"
                     placeholder="Name your group"
                     type="text"
                     {...field}
                     className="glassBorder dark:text-white dark:bg-black/10 bg-white text-black"
-                    ref={nameRef}
-                    defaultValue=''
+                    defaultValue=""
                   />
                 </FormControl>
                 <FormMessage />
@@ -191,16 +52,16 @@ const NewImageForm = () => {
           />
 
           <Button
-            ref={buttonRef}
+            id="submit-button"
             className="w-full disabled:bg-black-900/10 mt-6 dark:bg-white dark:text-black bg-black text-white "
             variant="black"
           >
-            {"Create Group"}
+            Create Group
           </Button>
         </form>
       </Form>
-    </DialogContent>
+    </OrgDialog>
   );
 };
 
-export default NewImageForm;
+export default CreateGroupModal;
