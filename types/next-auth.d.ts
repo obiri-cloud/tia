8b8cb90e6a6fc00fd6f0aa89005a9c49bf357735
tylerasa <sarpongasaresylvester@gmail.com>
2024-05-06 12:36:08 +0000

    feat: extending session gaps

---
+++ types/next-auth.d.ts
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      data :{
+        avatar: string; 
+        date_joined: string; 
+        email: string; 
+        first_name: string; 
+        is_active: boolean; 
+        is_admin: boolean; 
+        is_superuser: boolean; 
+        last_login: string; 
+        last_name: string; 
+        subscription_plan: string; 
+        username: string; 
+      },
+      message: string;
+      status: number;
+      tokens: {
+        access_token: string;
+        refresh_token: string;
+      }
+    };
+  }
+ 
+}
