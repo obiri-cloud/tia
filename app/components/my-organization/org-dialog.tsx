04ddb6fe696ec98976623b1946f8cc5049c99342
tylerasa <sarpongasaresylvester@gmail.com>
2024-05-10 12:08:26 +0000

    feat: changing the dialog to a sheet

--- app/components/my-organization/org-dialog.tsx
+++ app/components/my-organization/org-dialog.tsx
@@ -1,5 +1,6 @@
 import { ContentProps } from "@/app/types";
 import { DialogContent } from "@/components/ui/dialog";
+import { SheetContent } from "@/components/ui/sheet";
 import React, { ReactNode } from "react";
 
 const OrgDialog = ({
@@ -12,17 +13,15 @@ const OrgDialog = ({
   description?: string | undefined;
 }) => {
   return (
-    <div>
-      <DialogContent className=" overflow-y-scroll">
-        <div className="mb-6">
-          <h3 className="text-black dark:text-white mb-1 font-semibold text-lg">
-            {title}
-          </h3>
-          <div className="text-sm text-black dark:text-white ">{description}</div>
-        </div>
-        {children}
-      </DialogContent>
-    </div>
+    <SheetContent className=" overflow-y-scroll">
+      <div className="mb-6">
+        <h3 className="text-black dark:text-white mb-1 font-semibold text-lg">
+          {title}
+        </h3>
+        <div className="text-sm text-black dark:text-white ">{description}</div>
+      </div>
+      {children}
+    </SheetContent>
   );
 };
 
