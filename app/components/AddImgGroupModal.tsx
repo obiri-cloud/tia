49f287b01e071261833a60d0e41daeb4d8b79dfd
tylerasa <sarpongasaresylvester@gmail.com>
2024-05-10 12:01:44 +0000

    style: setting the layout for the sheet

--- app/components/AddImgGroupModal.tsx
+++ app/components/AddImgGroupModal.tsx
@@ -114,72 +114,82 @@ function AddImgGroupModal({
       title="Group Images"
       description="Select the Images you want to add to the group"
     >
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="image"
-            render={() => (
-              <FormItem>
-                {image &&
-                  image.map((item: ILabImage) => (
-                    <FormField
-                      key={item.id}
-                      control={form.control}
-                      name="image"
-                      render={({ field }) => {
-                        return (
-                          <FormItem
-                            key={item.id}
-                            className="flex flex-row items-start space-x-3 space-y-0"
-                          >
-                            <FormLabel className="flex items-center space-x-4">
-                              <Checkbox
-                                checked={field.value?.includes(item.id)}
-                                onCheckedChange={(checked: any) => {
-                                  console.log(checked, field);
-                                  return checked
-                                    ? field.onChange([...field.value, item.id])
-                                    : field.onChange(
-                                        field.value?.filter(
-                                          (value) => value !== item.id
-                                        )
-                                      );
-                                }}
-                              />
-                              <div className="bg-muted text-black dark:text-white font-bold p-4 w-4 h-4 flex justify-center items-center uppercase  rounded-full">
-                                {item.name[0]}
-                              </div>
-                              <div className="flex-1">
-                                <div className="font-medium text-black dark:text-white ">
-                                  {`${item.name} `}
-                                </div>
-                              </div>
-                            </FormLabel>
-                          </FormItem>
-                        );
-                      }}
-                    />
-                  ))}
-
-                {image?.length === 0 || !image ? (
-                  <p className="text-center text-black dark:text-white">
-                    No image in your organization...
-                  </p>
-                ) : null}
-              </FormItem>
-            )}
-          />
-          <Button
-            disabled={image?.length === 0 || !image ? true : false}
-            id="add-image-to-grp-button"
-            type="submit"
-            className="w-full"
+      <div className=" overflow-scroll flex flex-col flex-1 ">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-8 flex-1 overflow-scroll"
           >
-            Add Images
-          </Button>
-        </form>
-      </Form>
+            <FormField
+              control={form.control}
+              name="image"
+              render={() => (
+                <FormItem>
+                  {image &&
+                    image.map(
+                      (item: ILabImage) => (
+                        <FormField
+                          key={item.id}
+                          control={form.control}
+                          name="image"
+                          render={({ field }) => {
+                            return (
+                              <FormItem
+                                key={item.id}
+                                className="flex flex-row items-start space-x-3 space-y-0"
+                              >
+                                <FormLabel className="flex items-center space-x-4">
+                                  <Checkbox
+                                    checked={field.value?.includes(item.id)}
+                                    onCheckedChange={(checked: any) => {
+                                      console.log(checked, field);
+                                      return checked
+                                        ? field.onChange([
+                                            ...field.value,
+                                            item.id,
+                                          ])
+                                        : field.onChange(
+                                            field.value?.filter(
+                                              (value) => value !== item.id
+                                            )
+                                          );
+                                    }}
+                                  />
+                                  <div className="bg-muted text-black dark:text-white font-bold p-4 w-4 h-4 flex justify-center items-center uppercase  rounded-full">
+                                    {item.name[0]}
+                                  </div>
+                                  <div className="flex-1">
+                                    <div className="font-medium text-black dark:text-white ">
+                                      {`${item.name} `}
+                                    </div>
+                                  </div>
+                                </FormLabel>
+                              </FormItem>
+                            );
+                          }}
+                        />
+                      )
+                    )}
+
+                  {image?.length === 0 || !image ? (
+                    <p className="text-center text-black dark:text-white">
+                      No image in your organization...
+                    </p>
+                  ) : null}
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </div>
+      <Button
+        disabled={image?.length === 0 || !image ? true : false}
+        id="add-image-to-grp-button"
+        type="submit"
+        className="w-full"
+      >
+        Add Images
+      </Button>
     </OrgDialog>
   );
 }
