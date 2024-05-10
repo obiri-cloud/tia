b00779fcb296ceef03edde65effb292df7b71164
tylerasa <sarpongasaresylvester@gmail.com>
2024-05-10 14:10:49 +0000

    feat: switching to a sheet

--- app/components/AddImgGroupModal.tsx
+++ app/components/AddImgGroupModal.tsx
@@ -17,161 +17,165 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { useRouter } from "next/router";
-import { useRef, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { toast } from "@/components/ui/use-toast";
 import { useSession } from "next-auth/react";
 import OrgDialog from "./my-organization/org-dialog";
 import { GroupMember, ILabImage } from "../types";
+import { CheckedState } from "@radix-ui/react-checkbox";
+import { OrgGroup } from "../my-organization/groups/page";
 
-const FormSchema = z.object({
-  image: z.array(z.number()).refine((value) => value.some((item) => item), {
-    message: "You have to select at least one item.",
-  }),
-});
+export interface IImageChanges {
+  added: Set<string>;
+  removed: Set<string>;
+}
 
 function AddImgGroupModal({
-  image,
-  gid,
+  images,
+  group,
   onSubmit,
 }: {
-  image: ILabImage[] | undefined;
-  gid: number | undefined;
-  onSubmit: () => void;
+  images: ILabImage[] | undefined;
+  group: OrgGroup | undefined;
+  onSubmit: ( s: IImageChanges) => void;
 }) {
-  console.log("image", image);
-
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      image: [],
-    },
-  });
+  const form = useForm();
 
   const { data: session } = useSession();
   // @ts-ignore
   const token = session?.user!.tokens?.access_token;
 
-  async function handleSubmit(data: z.infer<typeof FormSchema>) {
-    let image_ids = { image_ids: data.image };
-    (
-      document.getElementById("add-image-to-grp-button") as HTMLButtonElement
-    ).disabled = true;
-    (
-      document.getElementById("add-image-to-grp-button") as HTMLButtonElement
-    ).textContent = "Adding Images...";
+  const [loadingImages, setIsLoadingImages] = useState<boolean>(false);
+  const [selectedImages, setSelectedImages] = useState(new Set());
 
-    let axiosConfig = {
-      method: "POST",
-      url: `${process.env.NEXT_PUBLIC_BE_URL}/organization/group/${gid}/image/add/`,
-      data: image_ids,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    };
+  const getImages = async () => {
+    setIsLoadingImages(true);
     try {
-      const response = await axios(axiosConfig);
-      if (response.status === 201 || response.status === 200) {
-        toast({
-          variant: "success",
-          title: `Image added to Group sucessfully`,
-          duration: 2000,
-        });
-        onSubmit();
-      } else {
-        toast({
-          variant: "destructive",
-          title: "image  adding  Error",
-          description: response.data,
-        });
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BE_URL}/organization/group/${group?.id}/image/list/`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            // @ts-ignore
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      let data = response.data.data;
+      console.log("data", data);
+
+      let newData = [];
+      let list = data[0].lab_image;
+      console.log("list", list);
+      
+
+      if (list.length > 0) {
+        newData = list ? list.map((d: any) => String(d.id)) : [];
       }
-    } catch (error: any) {
-      console.error("error", error);
-      const responseData = error.response.data;
-      if (error.response) {
-        toast({
-          variant: "destructive",
-          title: `${responseData.data}`,
-        });
-      } else {
-        toast({
-          variant: "destructive",
-          title: `${responseData.data}`,
-        });
-      }
-    } finally {
-      (
-        document.getElementById("add-image-to-grp-button") as HTMLButtonElement
-      ).disabled = false;
-      (
-        document.getElementById("add-image-to-grp-button") as HTMLButtonElement
-      ).textContent = "Add Images";
+
+      setSelectedImages(new Set(newData));
+
+      setIsLoadingImages(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoadingImages(false);
     }
-  }
+  };
+
+  useEffect(() => {
+    getImages();
+
+    return () => {
+      setSelectedImages(new Set());
+    };
+  }, [group]);
+
+  const [changes, setChanges] = useState<IImageChanges>({
+    added: new Set(),
+    removed: new Set(),
+  });
+
+  const handleCheckedChange = (checked: CheckedState, imageId: string) => {
+    console.log({ checked, imageId });
+
+    const updatedSet = new Set(selectedImages);
+    const updatedChanges = { ...changes };
+
+    if (checked) {
+      updatedSet.add(imageId);
+
+      updatedChanges.added.add(imageId);
+
+      if (updatedChanges.removed.has(imageId)) {
+        updatedChanges.removed.delete(imageId);
+      }
+    } else {
+      updatedSet.delete(imageId);
+
+      updatedChanges.removed.add(imageId);
+
+      if (updatedChanges.added.has(imageId)) {
+        updatedChanges.added.delete(imageId);
+      }
+    }
+
+    setSelectedImages(updatedSet);
+    setChanges(updatedChanges);
+  };
 
   return (
     <OrgDialog
-      title="Group Images"
+      title={`${group?.name} Images`}
       description="Select the Images you want to add to the group"
     >
       <div className=" overflow-scroll flex flex-col flex-1 ">
         <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(handleSubmit)}
-            className="space-y-8 flex-1 overflow-scroll"
-          >
+          <form className="space-y-8 flex-1 overflow-scroll">
             <FormField
               control={form.control}
               name="image"
               render={() => (
                 <FormItem>
-                  {image &&
-                    image.map(
-                      (item: ILabImage) => (
-                        <FormField
-                          key={item.id}
-                          control={form.control}
-                          name="image"
-                          render={({ field }) => {
-                            return (
-                              <FormItem
-                                key={item.id}
-                                className="flex flex-row items-start space-x-3 space-y-0"
-                              >
-                                <FormLabel className="flex items-center space-x-4">
-                                  <Checkbox
-                                    checked={field.value?.includes(item.id)}
-                                    onCheckedChange={(checked: any) => {
-                                      console.log(checked, field);
-                                      return checked
-                                        ? field.onChange([
-                                            ...field.value,
-                                            item.id,
-                                          ])
-                                        : field.onChange(
-                                            field.value?.filter(
-                                              (value) => value !== item.id
-                                            )
-                                          );
-                                    }}
-                                  />
-                                  <div className="bg-muted text-black dark:text-white font-bold p-4 w-4 h-4 flex justify-center items-center uppercase  rounded-full">
-                                    {item.name[0]}
+                  {images &&
+                    images.map((item: ILabImage) => (
+                      <FormField
+                        key={item.id}
+                        control={form.control}
+                        name="image"
+                        render={({ field }) => {
+                          return (
+                            <FormItem
+                              key={item.id}
+                              className="flex flex-row items-start space-x-3 space-y-0"
+                            >
+                              <FormLabel className="flex items-center space-x-4">
+                                <Checkbox
+                                  checked={selectedImages?.has(String(item.id))}
+                                  onCheckedChange={(checked) =>
+                                    handleCheckedChange(
+                                      checked,
+                                      String(item.id)
+                                    )
+                                  }
+                                />
+                                <div className="bg-muted text-black dark:text-white font-bold p-4 w-4 h-4 flex justify-center items-center uppercase  rounded-full">
+                                  {item.name[0]}
+                                </div>
+                                <div className="flex-1">
+                                  <div className="font-medium text-black dark:text-white ">
+                                    {`${item.name} `}
                                   </div>
-                                  <div className="flex-1">
-                                    <div className="font-medium text-black dark:text-white ">
-                                      {`${item.name} `}
-                                    </div>
-                                  </div>
-                                </FormLabel>
-                              </FormItem>
-                            );
-                          }}
-                        />
-                      )
-                    )}
+                                </div>
+                              </FormLabel>
+                            </FormItem>
+                          );
+                        }}
+                      />
+                    ))}
 
-                  {image?.length === 0 || !image ? (
+                  {images?.length === 0 || !images ? (
                     <p className="text-center text-black dark:text-white">
                       No image in your organization...
                     </p>
@@ -183,12 +187,13 @@ function AddImgGroupModal({
         </Form>
       </div>
       <Button
-        disabled={image?.length === 0 || !image ? true : false}
+        onClick={() => onSubmit( changes)}
+        disabled={images?.length === 0 || !images ? true : false}
         id="add-image-to-grp-button"
         type="submit"
         className="w-full"
       >
-        Add Images
+        Update Images List
       </Button>
     </OrgDialog>
   );
