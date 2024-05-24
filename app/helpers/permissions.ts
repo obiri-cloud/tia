1a00c376118b5b21a934806e54f5ed621d207781
tylerasa <sarpongasaresylvester@gmail.com>
2024-05-24 11:06:32 +0000

    chore: adding page description

--- app/helpers/permissions.ts
+++ app/helpers/permissions.ts
@@ -16,28 +16,34 @@ const permissionsArray: [Role, Permissions][] = [
           label: "Labs",
           link: "/my-organization",
           icon: PanelLeft,
+          description: "This page shows all the labs in your organizations.",
         },
         {
           label: "Groups",
           link: "/my-organization/groups",
           icon: GalleryVerticalEndIcon,
+          description: "This page shows all the groups in your organizations.",
         },
         {
           label: "Members",
           link: "/my-organization/members",
+
           icon: Users,
+          description: "This page shows all the members in your organizations.",
         },
         {
           label: "Invitation",
           link: "/my-organization/invitation",
           icon: TicketIcon,
+          description:
+            "This page shows all the invitations in your organizations.",
         },
         {
           label: "Organization Account",
           link: "/my-organization/account",
           icon: User,
-          position: "bottom"
-
+          position: "bottom",
+          description: "This page shows details about the organization",
         },
       ],
     },
@@ -50,11 +56,13 @@ const permissionsArray: [Role, Permissions][] = [
           label: "Labs",
           link: "/my-organization",
           icon: PanelLeft,
+          description: "This page shows all the labs in your organizations.",
         },
         {
           label: "Groups",
           link: "/my-organization/groups",
           icon: GalleryVerticalEndIcon,
+          description: "This page shows all the groups in your organizations.",
         },
         // {
         //   label: "Members",
@@ -65,6 +73,8 @@ const permissionsArray: [Role, Permissions][] = [
           label: "Invitation",
           link: "/my-organization/invitation",
           icon: TicketIcon,
+          description:
+            "This page shows all the invitations in your organizations.",
         },
       ],
     },
@@ -77,21 +87,26 @@ const permissionsArray: [Role, Permissions][] = [
           label: "Labs",
           link: "/my-organization",
           icon: PanelLeft,
+          description: "This page shows all the labs in your organizations.",
         },
         {
           label: "Groups",
           link: "/my-organization/groups",
           icon: GalleryVerticalEndIcon,
+          description: "This page shows all the groups in your organizations.",
         },
         {
           label: "Members",
           link: "/my-organization/members",
           icon: Users,
+          description: "This page shows all the members in your organizations.",
         },
         {
           label: "Invitation",
           link: "/my-organization/invitation",
           icon: TicketIcon,
+          description:
+            "This page shows all the invitations in your organizations.",
         },
       ],
     },
