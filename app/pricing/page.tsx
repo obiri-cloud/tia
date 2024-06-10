932c521e67620c433c2900390232a78a42cbfc5d
tylerasa <sarpongasaresylvester@gmail.com>
2024-06-10 12:31:10 +0000

    feat: pricing done

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Tiers from "./tiers";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 
 const Hero = () => {
+  const [plan, setPlan] = useState<string>("monthly");
+
   return (
     <div className="">
       <section className="lg:py-[80px] p-6  mt-[73px]">
@@ -10,11 +14,36 @@ const Hero = () => {
             Tialabs pricing plans that fit your needs
           </h1>
           <p className="text-left lg:text-center mt-6 lg:text-[18px] text-base leading-[150%]">
-            Our platform empowers learners with comprehensive online courses focused on Docker and various DevOps technologies, enabling them to enhance their skills in building, deploying, and managing software in a modern cloud infrastructure.
+            Our platform empowers learners with comprehensive online courses
+            focused on Docker and various DevOps technologies, enabling them to
+            enhance their skills in building, deploying, and managing software
+            in a modern cloud infrastructure.
           </p>
         </div>
+        <div className="flex justify-center">
+          <div className="mt-8 bg-white shadow-md inline-flex rounded-full text-sm p-1 gap-2 cursor-pointer transition duration-300 ease-in-out">
+            <div
+              onClick={() => setPlan("monthly")}
+              className={`
+              rounded-full text-black px-3 py-1 transition duration-300 ease-in-out
+              ${plan === "monthly" ? "bg-black text-white" : "bg-white"}
+              `}
+            >
+              Monthly
+            </div>
+            <div
+              onClick={() => setPlan("yearly")}
+              className={`
+              rounded-full text-black px-2 py-1 transition duration-300 ease-in-out
+              ${plan === "yearly" ? "bg-black text-white" : "bg-white"}
+              `}
+            >
+              Yearly
+            </div>
+          </div>
+        </div>
       </section>
-      <Tiers />
+      <Tiers plan={plan} />
     </div>
   );
 };
--- app/pricing/tiers.tsx
+++ app/pricing/tiers.tsx
@@ -1,57 +1,132 @@
 import React from "react";
-import { BadgeCheck, Circle } from "lucide-react";
+import { Check, Circle, X } from "lucide-react";
 
-const Tiers = () => {
+const Tiers = ({ plan }: { plan: string }) => {
   const essentials = {
-    title: "Essential",
+    title: "Basic Plan (Free)",
     text: "Sub-text description or not",
-    price: "$2",
+    price: "GHS 0",
     features: [
-      ...new Array(7).fill({
-        feature: "Feature",
-        icon: <BadgeCheck />,
+      {
+        feature: "Absolutely Free (GHC 0.00)",
+        icon: <Check className="text-green-400 w-4 h-4" />,
         available: true,
-      }),
-      ...new Array(3).fill({
-        feature: "Feature",
-        icon: <Circle />,
+      },
+      {
+        feature: "Access to some curated labs",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Lab Time Extension",
+        icon: <X className="text-red-400 w-4 h-4" />,
         available: false,
-      }),
+      },
+      {
+        feature: "Organization management",
+        icon: <X className="text-red-400 w-4 h-4" />,
+        available: false,
+      },
     ],
   };
   const premium = {
-    title: "Premium",
+    title: "Plus",
     text: "Sub-text description or not",
-    price: "$2",
+    price: plan === "monthly" ? "GHC 149" : "GHC 1430",
     features: [
-      ...new Array(7).fill({
-        feature: "Feature",
-        icon: <BadgeCheck />,
+      {
+        feature: "GHC 149/user/month or GHC 1430/yearly (at 20% off)",
+        icon: <Check className="text-green-400 w-4 h-4" />,
         available: true,
-      }),
-      ...new Array(3).fill({
-        feature: "Feature",
-        icon: <Circle />,
-        available: false,
-      }),
+      },
+      {
+        feature: "Access to all Labs including specially curated Labs",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Time Extension on Labs",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Manage Unlimited Users in Organization",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Run 5 concurrent Labs",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Proxy server for Labs to public internet",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Multi-Node Kubernetes Clusters",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Access to Sandbox environments",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Custom Labs environment upon request",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Business Support with dedicated Account Manager",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
     ],
   };
 
   const plus = {
-    title: "Plus",
+    title: "Standard",
     text: "Sub-text description or not",
-    price: "$2",
+    price: plan === "monthly" ? "GHC 129" : "GHC 1230",
     features: [
-      ...new Array(7).fill({
-        feature: "Feature",
-        icon: <BadgeCheck />,
+      {
+        feature: "GHC 129",
+        icon: <Check className="text-green-400 w-4 h-4" />,
         available: true,
-      }),
-      ...new Array(3).fill({
-        feature: "Feature",
-        icon: <Circle />,
-        available: false,
-      }),
+      },
+      {
+        feature: "Access to all Labs",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Time Extension on Labs",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Manage Unlimited Users in Organization",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Run 2 concurrent Labs",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Proxy server for Labs to public internet",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
+      {
+        feature: "Single Node Kubernetes Clusters",
+        icon: <Check className="text-green-400 w-4 h-4" />,
+        available: true,
+      },
     ],
   };
 
@@ -60,15 +135,15 @@ const Tiers = () => {
       <div className="container mx-auto lg:grid grid-cols-3 block items-end  gap-10 text-appBlack">
         {/* essentials */}
         <div className="p-8 bg-appGray rounded-2xl h-fit">
-          <h3 className="text-[38px] tracking-[1px] font-semibold text-appBlack leading-[120%] mb-[10px] ">
+          <h3 className="text-[18px] font-semibold text-appBlack leading-[120%] mb-[10px] ">
             {essentials.title}
           </h3>
-          <p className="leading-150 mb-6">{essentials.text}</p>
+          {/* <p className="leading-150 mb-6">{essentials.text}</p> */}
           <p>
             <span className="text-[30px] font-semibold  tracking-[-0.33px] mr-[4px]">
               {essentials.price}
             </span>
-            per verification
+            user/month
           </p>
           <ul className="mt-6 mb-[40px]">
             {essentials.features.map((ele, i) => (
@@ -91,24 +166,24 @@ const Tiers = () => {
           </a>
         </div>
 
-        {/* plus */}
+        {/* premium */}
         <div className=" bg-appGray rounded-2xl border-2 lg:my-0 my-6 border-appGold">
           <div className="bg-appGold py-[12px]  text-appBlack rounded-t-[14px]  text-center font-semibold ">
             RECOMMENDED
           </div>
           <div className="p-8 pt-[24px]">
-            <h3 className="text-[38px] tracking-[1px] font-semibold text-appBlack leading-[120%] mb-[10px] ">
-              {plus.title}
+            <h3 className="text-[18px] font-semibold text-appBlack leading-[120%] mb-[10px] ">
+              {premium.title}
             </h3>
-            <p className="leading-150 mb-6">{plus.text}</p>
+            {/* <p className="leading-150 mb-6">{premium.text}</p> */}
             <p>
               <span className="text-[30px] font-semibold  tracking-[-0.33px] mr-[4px]">
-                {plus.price}
+                {premium.price}
               </span>
-              per verification
+              user/month
             </p>
             <ul className="mt-6 mb-[40px]">
-              {plus.features.map((ele, i) => (
+              {premium.features.map((ele, i) => (
                 <li
                   key={i}
                   className={`mb-[8px] flex items-center leading-150 ${
@@ -129,20 +204,20 @@ const Tiers = () => {
           </div>
         </div>
 
-        {/* premium */}
+        {/* plus */}
         <div className="p-8 bg-appGray rounded-2xl h-fit">
-          <h3 className="text-[38px] tracking-[1px] font-semibold text-appBlack leading-[120%] mb-[10px] ">
-            {premium.title}
+          <h3 className="text-[18px] font-semibold text-appBlack leading-[120%] mb-[10px] ">
+            {plus.title}
           </h3>
-          <p className="leading-150 mb-6">{premium.text}</p>
+          {/* <p className="leading-150 mb-6">{plus.text}</p> */}
           <p>
             <span className="text-[30px] font-semibold  tracking-[-0.33px] mr-[4px]">
-              {premium.price}
+              {plus.price}
             </span>
-            per verification
+            user/month
           </p>
           <ul className="mt-6 mb-[40px]">
-            {premium.features.map((ele, i) => (
+            {plus.features.map((ele, i) => (
               <li
                 key={i}
                 className={`mb-[8px] flex items-center leading-150 ${
