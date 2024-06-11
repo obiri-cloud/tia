56fb39f409dcf7439e0bc5f906078c8062966f7b
tylerasa <sarpongasaresylvester@gmail.com>
2024-06-11 14:07:54 +0000

    feat: using the lab card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,6 +13,7 @@ import Link from "next/link";
 import { ChevronRight } from "lucide-react";
 import { useQuery } from "react-query";
 import AltRouteCheck from "../components/alt-route-check";
+import LabCard from "../components/LabCard";
 
 const UserPage = () => {
   const { data: session } = useSession();
@@ -20,8 +21,6 @@ const UserPage = () => {
   const router = useRouter();
 
   const token = session?.user.tokens?.access_token;
-
-
 
   const { data: images } = useQuery(["images"], () => getImages());
 
@@ -53,7 +52,7 @@ const UserPage = () => {
     <div className="">
       <div className="border-b dark:border-b-[#2c2d3c] border-b-whiteEdge flex justify-between items-center gap-2 p-2">
         <div className="flex items-center">
-          <span className="p-2 ">All Images</span>
+          <span className="p-2 ">All Labs</span>
           <ChevronRight className="w-[12px] dark:fill-[#d3d3d3] fill-[#2c2d3c] " />
         </div>
         <AltRouteCheck />
@@ -63,26 +62,27 @@ const UserPage = () => {
         <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 p-4 md:p-6">
           {images && images.length >= -1 ? (
             images.map((image: ILabImage, i: number) => (
-              <div
-                onClick={() => viewImage(image)}
-                key={i}
-                className={`lab-card rounded-2xl p-8 w-full pl-6 neu-shadow dark:bg-cardDarkBg dark:text-white dark:shadow-none bg-white cursor-pointer`}
-              >
-                <img
-                  src={image.image_picture ?? ""}
-                  alt=""
-                  className="w-[60px] h-[60px]"
-                />
-                <div className="mt-[40px] ">
-                  <h6 className="font-semibold leading-[140%] text-2xl app-text-clip h-[65px] max-h-[65px]">
-                    {image.name}
-                  </h6>
-                </div>
-                <span className="flex gap-[10px] items-center h-fit lg:mt-[36px] mt-[28px] font-medium ">
-                  <h5 className="leading-[150%] font-medium">Go to lab</h5>
-                  <Arrow className="pointer  -rotate-45 transition-all delay-150 dark:fill-white fill-black" />
-                </span>
-              </div>
+              // <div
+              //   onClick={() => viewImage(image)}
+              //   key={i}
+              //   className={`lab-card rounded-2xl p-8 w-full pl-6 neu-shadow dark:bg-cardDarkBg dark:text-white dark:shadow-none bg-white cursor-pointer`}
+              // >
+              //   <img
+              //     src={image.image_picture ?? ""}
+              //     alt=""
+              //     className="w-[60px] h-[60px]"
+              //   />
+              //   <div className="mt-[40px] ">
+              //     <h6 className="font-semibold leading-[140%] text-2xl app-text-clip h-[65px] max-h-[65px]">
+              //       {image.name}
+              //     </h6>
+              //   </div>
+              //   <span className="flex gap-[10px] items-center h-fit lg:mt-[36px] mt-[28px] font-medium ">
+              //     <h5 className="leading-[150%] font-medium">Go to lab</h5>
+              //     <Arrow className="pointer  -rotate-45 transition-all delay-150 dark:fill-white fill-black" />
+              //   </span>
+              // </div>
+              <LabCard key={i} lab={image} viewImage={viewImage} />
             ))
           ) : (
             <>
@@ -97,7 +97,9 @@ const UserPage = () => {
 
           {images && images.length === 0 ? (
             <div className="w-full flex justify-center  items-center col-span-12">
-              <p className="text-gray-600 dark:text-white">No images found...</p>
+              <p className="text-gray-600 dark:text-white">
+                No images found...
+              </p>
             </div>
           ) : null}
         </div>
