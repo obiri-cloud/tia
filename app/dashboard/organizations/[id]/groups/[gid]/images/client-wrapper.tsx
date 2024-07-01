9f6dca22775049c0b92a4dffe653b4bb2268c9b1
tylerasa <sarpongasaresylvester@gmail.com>
2024-07-01 09:33:19 +0000

    chore: cleanup

--- app/dashboard/organizations/[id]/groups/[gid]/images/client-wrapper.tsx
+++ app/dashboard/organizations/[id]/groups/[gid]/images/client-wrapper.tsx
@@ -6,20 +6,23 @@ import React from "react";
 const ClientWrapper = ({ token }: { token: string }) => {
   const params = useParams();
   const searchParams = useSearchParams();
-const pathname = usePathname()
-// console.log("pathname", pathname);
-
 
   const gid = params.gid;
   const id = params.id;
 
   const image = searchParams.get("image");
-  let baseUrl = `/user/org/${id}/group/${gid}/image/${image}`
+  let baseUrl = `/user/org/${id}/group/${gid}/image/${image}`;
   let labCreationUrl = `${baseUrl}/lab/create/`;
-  let redirectUrl = `/dashboard/organizations/${id}/groups/${gid}`
-  let timeoutRedirectUrl = `/dashboard/organizations/${id}/groups/${gid}?name=Heritage&group_name=Docker`
+  let redirectUrl = `/dashboard/organizations/${id}/groups/${gid}`;
+  let timeoutRedirectUrl = `/dashboard/organizations/${id}/groups/${gid}?name=Heritage&group_name=Docker`;
 
-  return <MainImagePage token={token} labCreationUrl={labCreationUrl} redirectUrl={redirectUrl} />;
+  return (
+    <MainImagePage
+      token={token}
+      labCreationUrl={labCreationUrl}
+      redirectUrl={redirectUrl}
+    />
+  );
 };
 
 export default ClientWrapper;
